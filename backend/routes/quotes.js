b4e3bd175d9202e97463563e34001b9c4f40f218
luca-thompson <lucathompson006@gmail.com>
2026-08-11 15:56:47 +1000

    added Frontend quote list and beta quote detail

--- backend/routes/quotes.js
+++ backend/routes/quotes.js
@@ -4,22 +4,88 @@ const router = express.Router();
 const db = require("../db.js");
 
 
-// 1. GET all quotes
-// URL: GET /quotes
+//get all
+
 router.get('/', (req, res) => {
     const sqlStatement = "SELECT * FROM quotes;"
 
-    res.send(db.prepare('SELECT * FROM quotes').all());
+    res.status(200).send(db.prepare(sqlStatement).all());
+
+    
 });
 
-// 2. GET a single quote by ID
-// URL: GET /quotes/1
+//get single quote by id
 router.get('/:id', (req, res) => {
+
     const quote = db.prepare('SELECT * FROM quotes WHERE id = ?').get(req.params.id);
     if (!quote) {
-        return res.status(404).send('Quote not found\n');
+        return res.status(404).send('Quote not found\n');return
     }
-    res.send(quote);
+
+    res.status(200).send(quote);
 });
+
+//post new quote to db
+router.post('/', (req, res) => {
+  const {
+    customer_name,
+    cover_type,
+    applicant1_age,
+    applicant1_cover_history,
+    applicant2_age,
+    applicant2_cover_history,
+    hospital_cover,
+    extras_cover,
+    payment_frequency,
+    annual_discount,
+    notes
+  } = req.body;
+
+
+  if (!customer_name || !cover_type || !hospital_cover || !extras_cover || !payment_frequency) {
+    return res.status(400).send('some fields null\n');
+  }
+
+  if (applicant1_age < 18 || applicant1_age > 100) {
+    return res.status(400).send('Applicant 1 age outside of range\n');
+  }
+
+  if (cover_type !== 'Single') {
+    // Couple / Family require applicant 2
+    if (!applicant2_age || !applicant2_cover_history) {
+      return res.status(400).send('Missing applicant two age or history\n');
+    }
+    if (applicant2_age < 18 || applicant2_age > 100) {
+      return res.status(400).send('Applicant 2 age outside of range\n');
+    }
+  }
+
+  if (annual_discount < 0 || annual_discount > 0.10) {
+    return res.status(400).send('Annual discount percentage must be between 0 and 10\n');
+  }
+
+  const insertStatement = db.prepare(`
+    INSERT INTO quotes (
+      customer_name, cover_type,
+      applicant1_age, applicant1_cover_history,
+      applicant2_age, applicant2_cover_history,
+      hospital_cover, extras_cover,
+      payment_frequency, annual_discount, notes
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `);
+
+  const result = insertStatement.run(
+    customer_name, cover_type,
+    applicant1_age, applicant1_cover_history,
+    applicant2_age, applicant2_cover_history,
+    hospital_cover, extras_cover,
+    payment_frequency, annual_discount, notes
+  );
+
+  const newQuote = db.prepare('SELECT * FROM quotes WHERE id = ?').get(result.lastInsertRowid);
+  res.status(201).send(newQuote);
+});
+
+//put/update existing quote
 
 module.exports = router;
